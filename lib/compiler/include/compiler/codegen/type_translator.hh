87bae873df456827dbef40bce4095dca38fab5ec
trevorswan11 <tcs94@case.edu>
2026-07-14 15:14:13 -0400

    feat: Add type translator header

---
+++ lib/compiler/include/compiler/codegen/type_translator.hh
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <ankerl/unordered_dense.h>
+#include <gsl/pointers>
+#include <stdx/utility.hh>
+
+#include "compiler/sema/type.hh"
+
+namespace llvm {
+
+class LLVMContext;
+class FunctionType;
+class Type;
+class StructType;
+class IntegerType;
+class PointerType;
+
+} // namespace llvm
+
+namespace ghoti::codegen {
+
+class type_translator {
+  public:
+    explicit type_translator(llvm::LLVMContext& context) noexcept : context_{context} {}
+    ~type_translator() = default;
+    MAKE_PINNED(type_translator);
+
+    MAKE_DEDUCING_GETTER(context);
+    [[nodiscard]] auto translate(const sema::type& type) -> llvm::Type*;
+    [[nodiscard]] auto translate_function_type(const sema::types::function& fn)
+        -> llvm::FunctionType*;
+    [[nodiscard]] auto translate_slice_type() -> llvm::StructType*;
+
+    [[nodiscard]] auto get_int8_ty() const noexcept -> llvm::IntegerType*;
+    [[nodiscard]] auto get_int32_ty() const noexcept -> llvm::IntegerType*;
+    [[nodiscard]] auto get_int64_ty() const noexcept -> llvm::IntegerType*;
+    [[nodiscard]] auto get_int1_ty() const noexcept -> llvm::IntegerType*;
+    [[nodiscard]] auto get_float_ty() const noexcept -> llvm::Type*;
+    [[nodiscard]] auto get_double_ty() const noexcept -> llvm::Type*;
+    [[nodiscard]] auto get_void_ty() const noexcept -> llvm::Type*;
+    [[nodiscard]] auto get_ptr_ty() const noexcept -> llvm::PointerType*;
+
+  private:
+    auto translate_slice(const sema::types::slice& s) -> llvm::Type*;
+    auto translate_array(const sema::types::array& a) -> llvm::Type*;
+    auto translate_struct(const sema::types::struct_t& s, const sema::type& original)
+        -> llvm::Type*;
+    auto translate_union(const sema::types::union_t& u) -> llvm::Type*;
+    auto translate_enum(const sema::types::enum_t& e) -> llvm::Type*;
+
+  private:
+    llvm::LLVMContext&                                           context_;
+    ankerl::unordered_dense::map<const sema::type*, llvm::Type*> struct_cache_;
+};
+
+} // namespace ghoti::codegen
