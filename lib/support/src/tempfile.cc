ead414cc4e86d61db52fc58463aebf95ee4f6aeb
trevorswan11 <tcs94@case.edu>
2026-08-16 13:19:48 -0400

    fix: Wrap std::filesystem::remove in a try-catch for tempfiles to prevent erroneous uncaught exception crashes

--- lib/support/src/tempfile.cc
+++ lib/support/src/tempfile.cc
@@ -1,7 +1,10 @@
 #include "support/tempfile.hh"
 
 #include <atomic>
+#include <exception>
 #include <filesystem>
+#include <fmt/ostream.h>
+#include <iostream>
 #include <random>
 #include <string_view>
 
@@ -15,13 +18,19 @@ namespace {
 const u64        seed{std::random_device{}()};
 std::atomic<u64> counter{0};
 
-} // namespace
-
-tempfile::tempfile(std::string_view tag) : path{make_temp_path(tag)} {
-    std::filesystem::remove(path);
+auto try_remove_path(const std::filesystem::path& path) -> void {
+    try {
+        std::filesystem::remove(path);
+    } catch (std::exception& e) {
+        fmt::println(std::cerr, "Failed to remove tempfile: {}", e.what());
+    }
 }
 
-tempfile::~tempfile() { std::filesystem::remove(path); }
+} // namespace
+
+tempfile::tempfile(std::string_view tag) : path{make_temp_path(tag)} { try_remove_path(path); }
+
+tempfile::~tempfile() { try_remove_path(path); }
 
 auto tempfile::make_temp_path(std::string_view tag) -> std::filesystem::path {
     const auto dir{std::filesystem::temp_directory_path()};
