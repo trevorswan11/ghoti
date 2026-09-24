395879bcd533a129432a26e135a056231bbd2b78
trevorswan11 <tcs94@case.edu>
2026-09-23 22:58:52 -0400

    refactor: Make Signedness enum a u1 to make IntInfo as small as possible

--- lib/compiler/src/sema/context.cc
+++ lib/compiler/src/sema/context.cc
@@ -265,6 +265,7 @@ auto inject_functions(symbol_table& prelude, type_pool& pool) -> void {
 constexpr std::string_view BUILTIN_MODULE_SOURCE{
 #include "builtin.gh.inc"
 };
+// recompile
 
 constexpr std::string_view BUILTIN_NAMESPACE{"builtin"};
 
